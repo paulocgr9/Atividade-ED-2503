44d8e04c0cf0ded8d7c6dabfde38f7d2ff0743b5
PEDRO SANTOS TOKAR <B51062@fgv.edu.br>
2024-03-25 08:17:27 -0300

    Adicionamos a questão 3 sobre progressao aritmetica

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using std::string;
 
 int pow(int, int);
 int fatorial(int);
+float progressaoAritmetica(float, float, int);
 
 int main() 
 {
@@ -15,6 +16,10 @@ int main()
     cout << iFatoriado << endl;
     int iFatoriado_2 = fatorial(0);
     cout << iFatoriado_2 << endl;
+    float fProgredido = progressaoAritmetica(1, 1, 100);
+    cout << fProgredido << endl;
+    float fProgredido_2 = progressaoAritmetica(2.5, 2.5, 4);
+    cout << fProgredido_2 << endl;
     return 0;
 }
 
@@ -40,4 +45,10 @@ int fatorial(int iNumero)
         iRetorno = iRetorno * i;
     }
     return iRetorno;
+}
+
+float progressaoAritmetica(float fInicial, float fRazao, int iTermos)
+{
+    float fFinal = fInicial + fRazao * (iTermos - 1);
+    return ((fInicial + fFinal) * iTermos) / 2;
 }
