111087a377b2c2a762aeb90c897665499f374c1e
PEDRO SANTOS TOKAR <B51062@fgv.edu.br>
2024-03-25 08:01:25 -0300

    Fizemos a primeira atividade, reproduzindo a funcao pow da biblioteca math

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@ using std::cout;
 using std::endl;
 using std::string;
 
-int main() {
+int pow(int, int);
+
+int main() 
+{
+    int iElevado = pow(2, 5);
+    cout <<  iElevado << endl;
     return 0;
+}
+
+int pow(int iBase, int iExpoente) 
+{
+    int iRetorno = iBase;
+    for (int i = 1; i < iExpoente; i++)
+    {
+        iRetorno = iRetorno * iBase;
+    }
+    return iRetorno;
 }
