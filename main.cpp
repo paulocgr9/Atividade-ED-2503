e4d7d2b39da689b41216567a5dd8d2037ff448e7
PEDRO SANTOS TOKAR <B51062@fgv.edu.br>
2024-03-25 08:08:52 -0300

    Adicionamos a questão 2 sobre fatoriais

--- main.cpp
+++ main.cpp
@@ -5,11 +5,16 @@ using std::endl;
 using std::string;
 
 int pow(int, int);
+int fatorial(int);
 
 int main() 
 {
     int iElevado = pow(2, 5);
     cout <<  iElevado << endl;
+    int iFatoriado = fatorial(5);
+    cout << iFatoriado << endl;
+    int iFatoriado_2 = fatorial(0);
+    cout << iFatoriado_2 << endl;
     return 0;
 }
 
@@ -19,6 +24,20 @@ int pow(int iBase, int iExpoente)
     for (int i = 1; i < iExpoente; i++)
     {
         iRetorno = iRetorno * iBase;
+    }
+    return iRetorno;
+}
+
+int fatorial(int iNumero)
+{
+    if (iNumero == 0)
+    {
+        return 1;
+    }
+    int iRetorno = iNumero;
+    for (int i = iNumero - 1; i > 0; i--)
+    {
+        iRetorno = iRetorno * i;
     }
     return iRetorno;
 }
